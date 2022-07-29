f83f220e77c278f166ea30683a1a9cc508dd1e19
lihq1403 <lihaiqing1994@163.com>
2022-07-29 10:51:53 +0800

    fix session with cookie

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -11,6 +11,6 @@ declare(strict_types=1);
  */
 return [
     'http' => [
-        \Hyperf\Session\Middleware\SessionMiddleware::class,
+        \App\Middleware\SessionMiddleware::class,
     ],
 ];
